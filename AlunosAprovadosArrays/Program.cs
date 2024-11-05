c25a497f7317840840f2b38c0f9d7da2014526b7
Gabriela <gabriela.priaro@gmail.com>
2024-11-05 10:19:27 +0000

    Adicionar arquivos de projeto.

---
+++ AlunosAprovadosArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlunosAprovadosArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
